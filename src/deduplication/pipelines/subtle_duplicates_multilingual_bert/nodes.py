ab26db268d6bd42e2637216cc15fed1317f482cb
antoine-palazz <antoine.palazzolo@insee.fr>
2023-03-09 17:25:57 +0000

    add bert model

---
+++ src/deduplication/pipelines/subtle_duplicates_multilingual_bert/nodes.py
@@ -0,0 +1,102 @@
+"""
+This is a boilerplate pipeline 'subtle_duplicates_tf_idf'
+generated using Kedro 0.18.6
+"""
+
+from deduplication.utils import (
+    create_stopwords_list,
+    find_subtle_duplicates_from_tokens
+)
+from nltk.stem import WordNetLemmatizer
+import pandas as pd
+import torch
+from tqdm import tqdm
+from transformers import BertTokenizer, BertModel
+import warnings
+
+tqdm.pandas()
+warnings.filterwarnings('ignore')
+
+
+def lemmatize_texts(
+    texts: pd.Series,
+    stopwords_list: list
+) -> pd.Series:
+    lem = WordNetLemmatizer()
+    lemmatized_texts = texts.progress_apply(
+        lambda x: ' '.join(
+            [lem.lemmatize(word) for word in x.split()
+             if word not in stopwords_list
+             ]
+        )
+    )
+
+    return lemmatized_texts
+
+
+def encode_text(
+    text: str,
+    tokenizer,
+    model
+):
+    input_ids = torch.tensor(
+        tokenizer.encode(
+            text,
+            add_special_tokens=True,
+            truncation=True
+        )
+    ).unsqueeze(0)
+    outputs = model(input_ids)
+    last_hidden_state = outputs.last_hidden_state
+    return last_hidden_state[0][0].detach().numpy()
+
+
+def tokenize_multilingual_bert(
+    lemmatized_texts: pd.Series
+) -> list:
+
+    tokenizer = BertTokenizer.from_pretrained('bert-base-multilingual-cased')
+    model = BertModel.from_pretrained('bert-base-multilingual-cased')
+
+    bert_texts = lemmatized_texts.progress_apply(
+        encode_text,
+        tokenizer,
+        model)
+
+    matrix_bert_texts = [list(x) for x in bert_texts]
+    return matrix_bert_texts
+
+
+def identify_subtle_duplicates(
+    data: pd.DataFrame,
+    languages_list: list,
+    concatenated_col_name: str = 'text',
+    description_col: str = 'description',
+    date_col: str = 'retrieval_date',
+    id_col: str = 'id',
+    chunk_size: int = 10000,
+    threshold_semantic: int = 0.95,
+    threshold_partial: int = 0.1
+) -> pd.DataFrame:
+
+    stopwords_list = create_stopwords_list(languages_list)
+    lemmatized_texts = lemmatize_texts(
+        data[concatenated_col_name],
+        stopwords_list
+        )
+    tokenized_texts = tokenize_multilingual_bert(lemmatized_texts)
+
+    duplicates = find_subtle_duplicates_from_tokens(
+        data,
+        tokenized_texts,
+        languages_list,
+        description_col,
+        date_col,
+        id_col,
+        chunk_size,
+        threshold_semantic,
+        threshold_partial
+    )
+
+    duplicates = pd.DataFrame(duplicates)
+    return(duplicates)
--- src/deduplication/pipelines/subtle_duplicates_tf_idf/nodes.py
+++ src/deduplication/pipelines/subtle_duplicates_tf_idf/nodes.py
@@ -3,22 +3,17 @@ This is a boilerplate pipeline 'subtle_duplicates_tf_idf'
 generated using Kedro 0.18.6
 """
 
-from nltk.corpus import stopwords
-import nltk
+from deduplication.utils import (
+    create_stopwords_list,
+    find_subtle_duplicates_from_tokens
+)
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.metrics.pairwise import cosine_similarity
 from tqdm import tqdm
 import warnings
 
-nltk.download('stopwords')
 tqdm.pandas()
 warnings.filterwarnings('ignore')
-
-
-def create_stopwords_list(languages_list: list) -> list:
-    stopwords_list = stopwords.words(languages_list)
-    return stopwords_list
 
 
 def tokenize_tf_idf(
@@ -34,71 +29,6 @@ def tokenize_tf_idf(
 
     tokenized_texts = vectorizer.fit_transform(texts)
     return tokenized_texts
-
-
-def compute_chunk_cosine_similarity(
-    tokenized_texts,
-    start: int,
-    end: int
-):
-
-    end = min(end, tokenized_texts.shape[0])
-    cosine_similarity_matrix = cosine_similarity(
-        X=tokenized_texts[start:end],
-        Y=tokenized_texts
-        )
-    return cosine_similarity_matrix
-
-
-def find_subtle_duplicates_from_tokens(
-    data: pd.DataFrame,
-    tokenized_texts,
-    languages_list: list,
-    description_col: str = 'description',
-    date_col: str = 'retrieval_date',
-    id_col: str = 'id',
-    chunk_size: int = 10000,
-    threshold_semantic: int = 0.95,
-    threshold_partial: int = 0.1
-) -> pd.DataFrame:
-
-    duplicates = []
-
-    for chunk_start in range(0, len(data), chunk_size):
-        similarity_matrix_chunk = compute_chunk_cosine_similarity(
-            tokenized_texts,
-            chunk_start,
-            chunk_start+chunk_size)
-        compteur_init = len(duplicates)
-
-        for i in tqdm(range(chunk_size)):
-            for j in range(chunk_start+i+1, len(data)):
-                if similarity_matrix_chunk[i][j] > threshold_semantic:
-                    if (data.loc[chunk_start+i, date_col] !=
-                            data.loc[j, date_col]):
-                        duplicates.append(
-                            {'id1': data.loc[chunk_start+i, id_col],
-                             'id2': data.loc[j, id_col], 'type': 'TEMPORAL'}
-                        )
-                    elif abs(
-                            len(data.loc[chunk_start+i, description_col]) -
-                            len(data.loc[j, description_col])
-                        ) / (1 + min(
-                            len(data.loc[chunk_start+i, description_col]),
-                            len(data.loc[j, description_col])
-                            )) > threshold_partial:
-                        duplicates.append(
-                            {'id1': data.loc[chunk_start+i, id_col],
-                             'id2': data.loc[j, id_col], 'type': 'PARTIAL'})
-                    else:
-                        duplicates.append(
-                            {'id1': data.loc[chunk_start+i, id_col],
-                             'id2': data.loc[j, id_col], 'type': 'SEMANTIC'})
-
-        compteur_end = len(duplicates)
-        print(compteur_end-compteur_init)
-
-    return(duplicates)
 
 
 def identify_subtle_duplicates(
