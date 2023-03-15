53a764033baf2c2c8e00ffdf0185c93b2e6eb6db
antoine-palazz <antoine.palazzolo@insee.fr>
2023-03-15 16:00:41 +0000

    add analysis past submissions

--- setup.sh
+++ setup.sh
@@ -1,4 +1,6 @@
 git config --global credential.helper store
 
-mc cp s3/apalazzolo/Deduplication/wi_dataset.csv data/01_raw/wi_dataset.csv
+mc cp s3/apalazzolo/Deduplication/data/wi_dataset.csv data/01_raw/wi_dataset.csv
 # mc cp s3/projet-dedup-oja/data/wi_dataset.csv data/01_raw/wi_dataset.csv
+
+mc cp -r s3/apalazzolo/Deduplication/past_submissions/ data/09_past_submissions
