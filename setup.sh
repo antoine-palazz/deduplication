49bf313d48d627db22a249002b9cfcce839f218f
antoine-palazz <antoine.palazzolo@insee.fr>
2023-03-15 14:16:30 +0000

    update setup

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
 git config --global credential.helper store
 
-mc cp s3/projet-dedup-oja/data/wi_dataset.csv data/01_raw/wi_dataset.csv
+mc cp s3/apalazzolo/Deduplication/wi_dataset.csv data/01_raw/wi_dataset.csv
+# mc cp s3/projet-dedup-oja/data/wi_dataset.csv data/01_raw/wi_dataset.csv
