0e0ef144f506717df1349f1c34960a328ccdb9df
antoine-palazz <antoine.palazzolo@insee.fr>
2023-03-16 10:31:09 +0000

    update requirements

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 git config --global credential.helper store
 
+pip install -r src/requirements.txt
+
 mc cp s3/apalazzolo/Deduplication/data/wi_dataset.csv data/01_raw/wi_dataset.csv
 # mc cp s3/projet-dedup-oja/data/wi_dataset.csv data/01_raw/wi_dataset.csv
 
