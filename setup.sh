a470a89a576ff66aedd9c72a0e9827603999509c
antoine-palazz <antoine.palazzolo@insee.fr>
2023-03-21 15:08:14 +0000

    add pre-commit

--- setup.sh
+++ setup.sh
@@ -5,3 +5,6 @@ pip install -r src/requirements.txt
 mc cp s3/apalazzolo/Deduplication/data/wi_dataset.csv data/01_raw/wi_dataset.csv
 
 mc cp -r s3/apalazzolo/Deduplication/past_submissions/ data/09_past_submissions
+
+pre-commit autoupdate
+pre-commit install
