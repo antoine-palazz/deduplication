0c74827117ec24dc4b7e21a872e2b104236b4b1c
antoine-palazz <Token>
2023-03-26 01:06:20 +0000

    condition temporal

--- src/deduplication/extras/utils.py
+++ src/deduplication/extras/utils.py
@@ -51,6 +51,12 @@ def differentiate_duplicates(
     if row_1.drop("id", axis=1) == row_2.drop("id", axis=1):
         return 'FULL'  # Obvious
 
+    if (
+        row_1.drop(["id", "retrieval_date"], axis=1) ==
+        row_2.drop(["id", "retrieval_date"], axis=1)
+    ):
+        return 'TEMPORAL'  # Obvious
+
     if row_1[language_col] == row_2[language_col]:
         lingual = "monolingual"
     else:
