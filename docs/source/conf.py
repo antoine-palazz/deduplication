3a264a625efd41eb9f7eddae6cc5fa07d0d8aca5
antoine-palazz <Token>
2023-04-04 08:38:56 +0000

    comment yaml and ipynb

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -97,7 +97,10 @@ html_theme = "sphinx_rtd_theme"
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-html_theme_options = {"collapse_navigation": False, "style_external_links": True}
+html_theme_options = {
+    "collapse_navigation": False,
+    "style_external_links": True
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
